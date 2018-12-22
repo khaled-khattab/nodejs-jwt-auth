4e651d9bb55cac8a1e0be76aac220afea5d4331a
khaled-khattab <khlaedadel.fci86@gmail.com>
2018-12-22 21:21:27 +0200

    user login api done

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -35,4 +35,27 @@ async function register(req, res, next){
   }
 }
 
-module.exports.register = register
+async function login(req, res, next){
+  try {
+    //validate request data
+    const schema = {
+      email: Joi.string().email().required(),
+      password: Joi.string().required()
+    };
+    const validator = Joi.validate(req.body, schema);
+    if(validator.error)
+      return res.status(400).json({message: validator.error.details[0].message});
+
+    //construct object and call the service method
+    var user = {
+      email: req.body.email,
+      password: req.body.password
+    };
+    const result = await User.login(user);
+    return res.status(result.status).json(result.data);
+  } catch (e) {
+    return res.status(500).json({message: "Error while logging in: "+ e});
+  }
+}
+
+module.exports = {register, login}
--- app/services/user.service.js
+++ app/services/user.service.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 async function register(user){
   const newUser = new User({
@@ -17,6 +19,25 @@ async function register(user){
   }
 }
 
+async function login(data){
+  try {
+    const user = await User.findOne({email: data.email});
+    if(user === null)
+      return {status: 401, data: {message: "Unauthorized Access"}};
+    const authenticated = await bcrypt.compare(data.password, user.password);
+    if(!authenticated)
+      return {status: 401, data: {message: "Unauthorized Access"}};
+    const JWTToken = jwt.sign({
+        email: user.email,
+        _id: user._id
+      }, 'secret', { expiresIn: '2h' } );
+    return {status: 200, data: {token: JWTToken}};
+  }
+  catch (e) {
+    throw new Error("Error while logging in: "+ e);
+  }
+}
+
 async function validateEmail(userEmail){
   try {
     const result = await User.find({email: userEmail});
@@ -29,4 +50,4 @@ async function validateEmail(userEmail){
   }
 }
 
-module.exports = {register, validateEmail};
+module.exports = {register, validateEmail, login};
